eea6491effea0498edd5383821e7642562908354
Saikal Asylbekova <saikalasylbekova@saikals-mbp.attlocal.net>
2020-07-16 13:46:19 -0500

    commit

---
+++ force-app/main/default/aura/appointments/appointmentsController.js
@@ -0,0 +1,40 @@
+({
+    doInit: function (component, event, helper) {
+        var action = component.get("c.pastAppointments");
+        action.setParams({ contactId : component.get("v.recordId") });
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.eventsList",response.getReturnValue());
+            }
+            else if (state === "INCOMPLETE") {
+                component.set("v.eventsList",null);
+            }
+            else if (state === "ERROR") {
+                component.set("v.eventsList",null);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    handlePubsubReady: function(component) {
+        var pubsub = component.find('pubsub');
+        var callback = $A.getCallback(function(data) {
+            alert(JSON.stringify(data));
+             /* component.set("v.lat", data.lat);
+            component.set("v.lon", data.lon);
+            component.set("v.name", data.name);
+            */
+           /* component.set('v.contactId', contactId);
+           var service = component.find('service');
+           service.reloadRecord(); */
+        });
+        pubsub.registerListener('locationSelected', callback);
+    },
+
+    handleDestroy: function(component) {
+        var pubsub = component.find('pubsub');
+        pubsub.unregisterAllListeners();
+    }
+})
